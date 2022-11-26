b2e6cc82a3eaf1784ab9222816b36924bbf63b29
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2022-11-26 14:59:50 +0100

    Add tikzfeynman

--- pyfeyn2/feynmandiagram.py
+++ pyfeyn2/feynmandiagram.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from decimal import Decimal
 from typing import List, Optional
 
 # from pyfeyn2.propagator import Propagator
@@ -6,34 +7,114 @@ from typing import List, Optional
 
 
 @dataclass
-class Vertex:
-    id: Optional[str] = field(
-        default="P1", metadata={"xml_attribute": True, "type": "Attribute"}
-    )
-    name: str = field(default=0, metadata={"xml_attribute": True, "type": "Attribute"})
-
-
-@dataclass
-class Propagator:
-    id: Optional[str] = field(
-        default="P1", metadata={"xml_attribute": True, "type": "Attribute"}
-    )
+class Particle:
     pdgid: Optional[int] = field(
         default=0, metadata={"xml_attribute": True, "type": "Attribute"}
     )
-    name: str = field(default=0, metadata={"xml_attribute": True, "type": "Attribute"})
+    type: Optional[str] = field(
+        default="", metadata={"xml_attribute": True, "type": "Attribute"}
+    )
+
+    def set_pdgid(self, pdgid):
+        self.pdgid = pdgid
+        # TODO get type from pdgid
+        return self
+
+    def set_type(self, type):
+        self.type = type
+        return self
+
+
+@dataclass
+class Identifiable:
+    id: Optional[str] = field(
+        default="", metadata={"xml_attribute": True, "type": "Attribute"}
+    )
+
+
+@dataclass
+class Labeled:
+    label: Optional[str] = field(
+        default="", metadata={"xml_attribute": True, "type": "Attribute"}
+    )
+
+
+@dataclass
+class Point:
+    x: Optional[Decimal] = field(
+        default=0.0, metadata={"xml_attribute": True, "type": "Attribute"}
+    )
+    y: Optional[Decimal] = field(
+        default=0.0, metadata={"xml_attribute": True, "type": "Attribute"}
+    )
+
+    def set_xy(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+        return self
+
+
+@dataclass
+class Targeting:
+    target: Optional[str] = field(
+        default="", metadata={"xml_attribute": True, "type": "Attribute"}
+    )
+
+    def set_target(self, target):
+        self.target = target.id
+        return self
+
+
+@dataclass
+class Sourcing:
+    source: Optional[str] = field(
+        default="", metadata={"xml_attribute": True, "type": "Attribute"}
+    )
+
+    def set_source(self, source):
+        self.source = source.id
+        return self
+
+
+@dataclass
+class Line(Targeting, Sourcing):
+    def connect(self, source, target):
+        self.set_source(source)
+        self.set_target(target)
+        return self
+
+
+@dataclass
+class Vertex(Labeled, Point, Identifiable):
+    pass
+
+
+@dataclass
+class Leg(Labeled, Particle, Point, Targeting, Identifiable):
+    sense: str = field(
+        default="incoming", metadata={"xml_attribute": True, "type": "Attribute"}
+    )
+
+
+@dataclass
+class Propagator(Labeled, Particle, Line, Identifiable):
+    pass
 
 
 @dataclass
 class FeynmanDiagram:
     class Meta:
-        name = "FeynmanDiagram"
+        name = "feynmandiagram"
 
     propagators: List[Propagator] = field(
         default_factory=list,
-        metadata={"name": "Propagator", "type": "Element", "namespace": ""},
+        metadata={"name": "propagator", "type": "Element", "namespace": ""},
     )
     vertices: List[Vertex] = field(
         default_factory=list,
-        metadata={"name": "Vertex", "type": "Element", "namespace": ""},
+        metadata={"name": "vertex", "type": "Element", "namespace": ""},
+    )
+    legs: List[Leg] = field(
+        default_factory=list,
+        metadata={"name": "leg", "type": "Element", "namespace": ""},
     )
--- tests/test_xml.py
+++ tests/test_xml.py
@@ -3,15 +3,21 @@ from xsdata.formats.dataclass.serializers import XmlSerializer
 from xsdata.formats.dataclass.serializers.config import SerializerConfig
 
 from pyfeyn2 import io_utils
-from pyfeyn2.feynmandiagram import FeynmanDiagram, Propagator, Vertex
+from pyfeyn2.feynmandiagram import FeynmanDiagram, Leg, Propagator, Vertex
 
 
 def test_print_as_xml():
     fd = FeynmanDiagram()
-    fd.propagators.append(Propagator())
-    fd.propagators.append(Propagator())
-    fd.vertices.append(Vertex())
-    fd.vertices.append(Vertex())
+    v1 = Vertex("v1")
+    v2 = Vertex("v2")
+    p1 = Propagator("p1")
+    l1 = Leg("l1")
+    p1.set_source(v1)
+    p1.set_target(v2)
+    fd.propagators.append(p1)
+    fd.vertices.append(v1)
+    fd.vertices.append(v2)
+    fd.legs.append(l1)
 
     config = SerializerConfig(pretty_print=True)
     serializer = XmlSerializer(config=config)
