9b3dff877759433fa20f5c429d57ed6d5df00b9a
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2022-12-11 17:26:28 +0100

    test ascii

---
+++ tests/test_text.py
@@ -0,0 +1,27 @@
+from pyfeyn2.render.latex.dot import feynman_adjust_points
+from pyfeyn2.render.latex.tikzfeynman import TikzFeynmanRender
+from pyfeyn2.render.text.ascii import ASCIIRender
+from pyfeyn2.render.text.unicode import UnicodeRender
+from tests.test_feynman import test_gluons, test_many_gluons
+
+
+def test_ascii():
+    fd = test_many_gluons()
+    fd = feynman_adjust_points(fd)
+
+    tfd = ASCIIRender(fd)
+    tfd.render()
+    print(tfd.get_src_txt())
+
+
+def test_unicode():
+    fd = test_many_gluons()
+    fd = feynman_adjust_points(fd)
+
+    tfd = UnicodeRender(fd)
+    tfd.render()
+    print(tfd.get_src_txt())
+
+
+test_ascii()
+test_unicode()
