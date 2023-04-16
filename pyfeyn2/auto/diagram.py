bf613e9013453d50691685f35990014cc1737ed0
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2023-04-16 14:01:16 +0200

    Fix imports

--- pyfeyn2/auto/diagram.py
+++ pyfeyn2/auto/diagram.py
@@ -1,15 +1,7 @@
-from feynmodel.interface.qgraf import feynmodel_to_qgraf, qgraf_to_feynmodel
-from feynmodel.interface.ufo import load_ufo_model
-from pyqgraf import qgraf
-from xsdata.formats.dataclass.parsers import XmlParser
-
 from pyfeyn2.auto.bend import auto_bend
 from pyfeyn2.auto.label import auto_label
 from pyfeyn2.auto.position import feynman_adjust_points
-from pyfeyn2.feynmandiagram import FeynmanDiagram, FeynML
-from pyfeyn2.render.latex.feynmp import FeynmpRender
-from pyfeyn2.render.latex.tikzfeynman import TikzFeynmanRender
-from pyfeyn2.render.pyx.pyxrender import PyxRender
+from pyfeyn2.feynmandiagram import FeynmanDiagram
 
 
 def auto_diagram(fd: FeynmanDiagram, scale=2, size=15):
@@ -27,8 +19,3 @@ def auto_diagram(fd: FeynmanDiagram, scale=2, size=15):
     auto_label(d.propagators)
     auto_label(d.legs)
     return d
-    # print(d)
-    t = TikzFeynmanRender(d)
-    # print(t.get_src())
-    t.render(show=True)
-    # break
