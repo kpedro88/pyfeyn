d4b9d069cc07bb388c151fd6927176e926859aed
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2023-07-24 09:03:45 +0200

    rm help test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,5 @@
 from pyfeyn2.mkfeyndiag import main
 
 
-def test_main_help(capsys):
-    main(["--help"])
-    captured = capsys.readouterr()
-    assert "Draw FeynML diagrams with pyfeyn2." in captured.out
-
-
 def test_main_tikz(capsys):
     main(["tests/test.fml", "-r", "tikz"])
